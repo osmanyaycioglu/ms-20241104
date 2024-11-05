2e3fd68cd71675fd688036ac8ecbee54ac8d76f9
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-11-05 11:40:32 +0300

    2

---
+++ ms-order/src/main/java/org/training/microservice/msorder/MsOrderApplication.java
@@ -0,0 +1,14 @@
+package org.training.microservice.msorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsOrderApplication.class,
+                              args);
+    }
+
+}
