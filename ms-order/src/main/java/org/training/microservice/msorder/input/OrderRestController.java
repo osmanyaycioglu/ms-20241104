9b149e2093b51b7c0216323e3c5f6eb85c1fb008
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-11-08 15:51:24 +0300

    5

--- ms-order/src/main/java/org/training/microservice/msorder/input/OrderRestController.java
+++ ms-order/src/main/java/org/training/microservice/msorder/input/OrderRestController.java
@@ -1,12 +1,14 @@
 package org.training.microservice.msorder.input;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/order")
+@RefreshScope
 public class OrderRestController {
 
     @Value("${a.b.c}")
